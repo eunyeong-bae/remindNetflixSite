f751fb203bd06825006dd7ba295dd1f0c4616cbb
배은영 <adcadcad@naver.com>
2023-09-18 14:46:07 +0900

    1.개발환경셋팅 1.1 라우터 셋팅(멀티페이지 구현) 1.2 리덕스 셋팅(state 글로벌하게 관리화기 위해) 1.3 리덕스 미들웨어 셋팅(비동기 작업 처리를 위해) 1.4 멀티 페이지 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetail from './pages/MovieDetail';
+/**
+ * 1. 3개 페이지 필요(홈페이지, movie 홈페이지, movieDetail 페이지)
+ * 2. 홈페이지에서 배너 볼 수 있음
+ * 3. 3가지 섹션의 영화 볼 수 있음(인기영화, 순위높은영화, 업커밍영화)
+ * 4. 각 영화에 마우스를 올리면 제목/장르/점수/인기도.청불여부 알 수있음
+ * 5. 영화를 슬라이드로 넘기면서 볼 수 있다
+ * 
+ * 6. 영호 ㅏ디테일 페이지에서 정보를 볼 수있음(포스터,제목,ㅈ루거리,점수,.인기도 등)
+ * 7. 트레일러(예고편) 누르면 예고편 볼 수 있음
+ * 8. 영화 리뷰도 볼 수 있음
+ * 9. 관련 영화도 볼 수 있음
+ * 
+ * 10.영화검색가능
+ * 11.영화정렬가능
+ * 12/영화 필터링 가능
+*/
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movies' element={<Movies />} />
+      <Route path='/movies/:id' element={<MovieDetail />} />
+    </Routes>
   );
 }
 
